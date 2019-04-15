c5f0e6b0a7dc84872b33bca43128f4ad4f2c87fd
codemass <worklette@yandex.ru>
2019-04-16 00:56:58 +0300

    Добавлены модели и контекст

---
+++ MessageApp/Models/SendMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageApp.Models
+{
+    public class SendMessage
+    {
+        public int MessageId { get; set; }
+        public DateTime DateTime { get; set; }
+        public int FromUser { get; set; }
+        public int ToUser { get; set; }
+    }
+}
