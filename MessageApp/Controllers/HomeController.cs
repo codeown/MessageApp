682a48979bb66cb30a2e5156e544fcf06743e20d
codemass <worklette@yandex.ru>
2019-04-16 03:14:19 +0300

    В HomeController изменен метод About. В представлении About изменен код.

--- MessageApp/Controllers/HomeController.cs
+++ MessageApp/Controllers/HomeController.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MessageApp.Models;
 
 namespace MessageApp.Controllers
 {
     public class HomeController : Controller
     {
+        MessageContext messageContext = new MessageContext();  //Добавляем для обращения к БД
+
         public ActionResult Index()
-        {
+        {            
             return View();
         }
-
+        
+        //===========================
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //Извлекаем данные из  таблицы Users
+            IEnumerable<User> users = messageContext.Users;
+
+            //Записываем Users в динамического свойство ViewBag
+            ViewBag.Users = User;
 
             return View();
         }
